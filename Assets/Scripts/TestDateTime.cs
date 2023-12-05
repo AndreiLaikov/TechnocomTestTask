0df2ed049e34649accda584bbfe511e4e6b33bbb
Андрей В. Лайков <lay4ik@gmail.com>
2023-12-05 10:36:02 +0300

    Request server DateTime

---
+++ Assets/Scripts/TestDateTime.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDateTime : MonoBehaviour
+{
+    [ContextMenu("GetTime")]
+   public void GetDateTime()
+    {
+        var serverTime = WorldTime.GetWorldTime();
+        DateTime localtime = DateTime.Now;
+        Debug.Log("Server: "+serverTime.Date);
+        Debug.Log("Local: "+localtime.Date);
+    }
+}
---
+++ Assets/Scripts/WorldTime.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Globalization;
+using System.Net;
+using UnityEngine;
+
+public class WorldTime : MonoBehaviour
+{
+    public static DateTime GetWorldTime()
+    {
+            var myHttpWebRequest = (HttpWebRequest)WebRequest.Create("http://www.google.com");
+            var response = myHttpWebRequest.GetResponse();
+            string todaysDates = response.Headers["date"];
+            return DateTime.ParseExact(todaysDates,
+                                       "ddd, dd MMM yyyy HH:mm:ss 'GMT'",
+                                       CultureInfo.InvariantCulture.DateTimeFormat,
+                                       DateTimeStyles.AssumeUniversal);
+    }
+}
